e32b324f5f029ba146c15a94709b6673983d6cb2
Jonathan Samuels <jonathansamuels@Jonathans-MacBook-Pro-2.local>
2024-08-19 19:49:12 -0700

    How to import multiple things from a file

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import pi, {doublePi, triplePi} from "./math.js"; 
 
-ReactDOM.render(
-  <ul>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
-  </ul>,
-  document.getElementById("root")
-);
+const root = ReactDOM.createRoot(document.getElementById("root")); 
 
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
+
+root.render(
+  <React.StrictMode>
+    <ul>
+      <li>{pi}</li>
+      <li>{doublePi}</li>
+      <li>{triplePi}</li>
+    </ul>
+  </React.StrictMode>
+  ); 
